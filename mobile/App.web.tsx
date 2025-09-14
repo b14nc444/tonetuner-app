2c0957eb0dbfef9c97306d24a0b09c3fa668630b
b14nc444 <b14nc4@naver.com>
2025-09-14 17:29:52 +0900

    refactor: 프로젝트 구조 통합 - 웹/모바일 코드를 mobile 폴더로 통합
    - 웹 관련 코드(src/, public/)를 mobile 폴더로 이동
    - package.json 통합으로 의존성 충돌 해결
    - 웹/모바일 각각의 진입점 분리 (App.web.tsx, index.web.tsx)
    - 루트 폴더의 중복 파일들 정리
    - 단일 node_modules로 모듈 해석 에러 방지

---
+++ mobile/App.web.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./src/App.css";
+import { MainScreen } from "./src/screens/MainScreen";
+
+function App() {
+  return (
+    <div className="App">
+      <MainScreen />
+    </div>
+  );
+}
+
+export default App;
